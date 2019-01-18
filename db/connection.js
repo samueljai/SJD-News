5b2f338a2ed95917a6b5a1101be0ad0be25a1a42
Samuel Jai Desai <samueljdesai@Samuels-MacBook-Pro.local>
2019-01-18 15:23:13 +0000

    log the environment

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,5 @@
 const ENV = process.env.NODE_ENV || 'development';
+console.log(ENV, 'node environment')
 const config = ENV === 'production' ? { client: 'pg', connection: process.env.DATABASE_URL } : require('../knexfile')[ENV];
 
 module.exports = require('knex')(config);
