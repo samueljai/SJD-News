1988d1d1741eb49952c8e1b1544c4f1778988d36
Samuel Jai Desai <samueljdesai@Samuels-MacBook-Pro.local>
2019-01-18 12:59:14 +0000

    deploying on heroku

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,6 @@ const knex = require('knex');
 const config = require('../knexfile');
 
 const ENV = process.env.NODE_ENV || 'development';
+const config = ENV === 'production' ? { client: 'pg', connection: process.env.DATABASE_URL } : require('../knexfile')[ENV];
 
-const connection = knex(config[ENV]);
-
-module.exports = connection;
+module.exports = require('knex')(config);
