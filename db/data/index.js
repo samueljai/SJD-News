9e53967b631976b07809283e4ab26afbec7f7342
Samuel Jai Desai <samueljdesai@Samuels-MacBook-Pro.local>
2019-01-18 15:10:14 +0000

    Initial commit

--- db/data/index.js
+++ db/data/index.js
@@ -1,7 +1,7 @@
 const test = require('./test-data');
 const development = require('./development-data');
 
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV === 'test' ? 'test' : 'development';
 
 const data = { test, development };
 
