0b112447d7b5269730d7ca28d2e13e559f6fae27
Samuel Jai Desai <samueljdesai@Samuels-MacBook-Pro.local>
2019-01-22 01:06:22 +0000

    added cors

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-
+const cors = require('cors');
 const app = require('express')();
 const bodyParser = require('body-parser');
 const apiRoute = require('./routes/apiRoute');
 const { handle404, handle400, handle422 } = require('./errors');
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use('/api', apiRoute);
 
 app.use(handle400);
